2dc6e1b4aad44024cb8424d037f5aec42530c4a9
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-07-03 16:15:18 +0100

    Evaluates annotation on all the objects

--- pkg/patterns/addon/pkg/status/preflight.go
+++ pkg/patterns/addon/pkg/status/preflight.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 
 	"github.com/blang/semver"
-	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	//"sigs.k8s.io/controller-runtime/pkg/log"
-	appsv1beta1 "sigs.k8s.io/application/api/v1beta1"
+
 	addonsv1alpha1 "sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon/pkg/apis/v1alpha1"
 	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative"
 	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative/pkg/manifest"
@@ -28,44 +27,48 @@ type preflightChecks struct {
 	version string
 }
 
-func (p *preflightChecks) Preflight(ctx context.Context, src declarative.DeclarativeObject, objs *manifest.Objects) error {
+func (p *preflightChecks) Preflight(
+	ctx context.Context,
+	src declarative.DeclarativeObject,
+	objs *manifest.Objects,
+) error {
 	log := log.Log
+	baseVersion := semver.Version{}
 	var versionNeededStr string
+	maxVersion := baseVersion
 
-	// get Annotation from Application resource
+	// get Annotation from any resource[Do we just want to do this on Applicationn?]
 	for _, obj := range objs.Items {
-		if obj.Kind == "Application" {
-			newApp := appsv1beta1.Application{}
+		// if obj.Kind == "Application" {
+		// newApp := appsv1beta1.Application{}
 
-			unstruct := obj.UnstructuredObject().Object
-			err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstruct, &newApp)
+		unstruct := obj.UnstructuredObject().Object
+		metadata := unstruct["metadata"].(map[string]interface{})
+		annotations, ok := metadata["annotations"].(map[string]interface{})
+		if ok {
+			versionNeededStr, _ = annotations["addons.k8s.io/operator-version"].(string)
+			log.WithValues("version", versionNeededStr).Info("Got version, %v")
 
+			versionActual, err := semver.Make(versionNeededStr)
 			if err != nil {
-				log.WithValues("Object", unstruct).Info("Unable to unmarshall Application, %v", err)
+				log.WithValues("version", versionNeededStr).Info("Unable to convert string to version, skipping this object")
 				continue
 			}
 
-			// metadata := unstruct["metadata"]["annotations"][]
-
-			versionNeededStr = newApp.ObjectMeta.Annotations["addons.k8s.io/operator-version"]
-
+			if versionActual.GT(maxVersion) {
+				maxVersion = versionActual
+			}
 		}
 	}
-
-	if versionNeededStr != "" {
-		versionNeeded, err := semver.Make(versionNeededStr)
+	if !maxVersion.Equals(baseVersion) {
 		// TODO(somtochi): Do we want to return an error when the version is invalid or just skip and use the operator?
-		if err != nil {
-			log.WithValues("version", versionNeededStr).Info("Unable to convert string to version, skipping check")
-			return nil
-		}
 		operatorVersion, err := semver.Make(p.version)
 		if err != nil {
 			log.WithValues("version", p.version).Info("Unable to convert string to version, skipping check")
 			return nil
 		}
 
-		if versionNeeded.GT(operatorVersion) {
+		if maxVersion.GT(operatorVersion) {
 			addonObject, ok := src.(addonsv1alpha1.CommonObject)
 			if !ok {
 				return fmt.Errorf("object %T was not an addonsv1alpha1.CommonObject", src)
@@ -74,12 +77,12 @@ func (p *preflightChecks) Preflight(ctx context.Context, src declarative.Declara
 			status := addonsv1alpha1.CommonStatus{
 				Healthy: false,
 				Errors: []string{
-					fmt.Sprintf("Addons needs version %v, this operator is version %v", versionNeeded.String(), operatorVersion.String()),
+					fmt.Sprintf("Addons needs version %v, this operator is version %v", maxVersion.String(), operatorVersion.String()),
 				},
 			}
 			addonObject.SetCommonStatus(status)
 
-			return fmt.Errorf("Operator not qualified, manifest needs operator >= %v", versionNeeded.String())
+			return fmt.Errorf("Operator not qualified, manifest needs operator >= %v", maxVersion.String())
 		}
 	}
 
