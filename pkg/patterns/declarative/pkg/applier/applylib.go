e596884328f756a295bbae2952303f185f5c6389
Justin SB <justinsb@google.com>
2022-10-18 20:28:38 -0400

    applylib: Recognize and map the --force kubectl option
    We use the force option quite a lot, so we need to support it.

--- pkg/patterns/declarative/pkg/applier/applylib.go
+++ pkg/patterns/declarative/pkg/applier/applylib.go
@@ -28,8 +28,13 @@ func (a *ApplySetApplier) Apply(ctx context.Context, opt ApplierOptions) error {
 		return fmt.Errorf("error parsing manifest: %w", err)
 	}
 
+	patchOptions := a.patchOptions
+
 	for _, arg := range opt.ExtraArgs {
 		switch arg {
+		case "--force":
+			force := true
+			patchOptions.Force = &force
 
 		default:
 			return fmt.Errorf("extraArg %q is not supported by the ApplySetApplier", arg)
@@ -44,7 +49,7 @@ func (a *ApplySetApplier) Apply(ctx context.Context, opt ApplierOptions) error {
 	restMapper := opt.RESTMapper
 
 	options := applyset.Options{
-		PatchOptions: a.patchOptions,
+		PatchOptions: patchOptions,
 		RESTMapper:   restMapper,
 		Client:       dynamicClient,
 	}
